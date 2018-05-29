94c252be70073b9d7c48064a483e2ca96e6d57b5
gongjinwei <jwakx@163.com>
2018-05-29 11:34:16 +0800

    change cos

--- qcloudcos/cos_object.py
+++ qcloudcos/cos_object.py
@@ -13,20 +13,20 @@ class CosObject(object):
 
     def get_object(self, name, is_private=False):
         method = 'get'
-        url, Authorization = self.make_params(method, name,is_private)
-        
+        url, authorization = self.make_params(method, name, is_private)
+
         s = requests.Session()
         if is_private:
-            s.headers.update(Authorization)
+            s.headers.update(authorization)
 
         r = s.get(url)
         return r
 
     def put_object(self, name, content):
         method = 'put'
-        url, Authorization = self.make_params(method, name)
+        url, authorization = self.make_params(method, name)
         s = requests.Session()
-        s.headers.update(Authorization)
+        s.headers.update(authorization)
 
         r = s.put(url, data=content)
         if r.status_code == 200:
@@ -34,49 +34,46 @@ class CosObject(object):
         else:
             LOGGER.info(r.content)
 
-
     def head_object(self, name, is_private=False):
         method = 'head'
 
-        url, Authorization = self.make_params(method, name,is_private)
-        
+        url, authorization = self.make_params(method, name, is_private)
+
         s = requests.Session()
         if is_private:
-            s.headers.update(Authorization)
+            s.headers.update(authorization)
 
         r = s.head(url)
         return r
 
-
     def delete_object(self, name):
         method = 'delete'
-        url,Authorization=self.make_params(method,name)
-        
+        url, authorization = self.make_params(method, name)
+
         s = requests.Session()
-        s.headers.update(Authorization)
+        s.headers.update(authorization)
 
         r = s.delete(url)
         if r.status_code == 204:
             return True
 
-
-    def make_params(self,method,name,is_private=True):
+    def make_params(self, method, name, is_private=True):
         appid = self.option['Appid']
         SecretID = self.option['SecretID']
         SecretKey = self.option['SecretKey']
         region = self.option['region']
         bucket = self.option['bucket']
         if name is None:
-            name=''
+            name = ''
         elif name[0] != '/':
             name = '/' + name
         objectName = name
-        
+
         url = "http://%s-%s.%s.myqcloud.com%s" % (bucket, appid, region, objectName)
         if is_private:
             auth = Auth(appid, SecretID, SecretKey, bucket, region, method, objectName)
-            Authorization = {'Authorization': auth.get_authorization()}
+            authorization = {'Authorization': auth.get_authorization()}
         else:
-            Authorization=None
-        
-        return url,Authorization
+            authorization = None
+
+        return url, authorization
