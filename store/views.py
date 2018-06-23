8097d1e27a20ef4956469533b1a7cdd86894a2b9
gongjinwei <jwakx@163.com>
2018-06-23 11:29:57 +0800

    审核店铺

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import Response
+from rest_framework.views import Response,status
 from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
@@ -10,3 +10,15 @@ class CheckApplicationViewSets(ModelViewSet):
     queryset = models.CheckApplication.objects.all()
     serializer_class = serializers.CheckApplicationSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        application=serializer.validated_data['application']
+        if application.application_status==1:
+            application.application_status=serializer.validated_data['apply_status']
+            application.save()
+        else:
+            return Response('状态无法被更改',status=status.HTTP_400_BAD_REQUEST)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
