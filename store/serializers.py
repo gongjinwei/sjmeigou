0f26d04e9d10ab620c34d26fcd4760b3d2981c85
gongjinwei <jwakx@163.com>
2018-06-25 16:56:51 +0800

    返回申请ID

--- store/serializers.py
+++ store/serializers.py
@@ -10,10 +10,6 @@ class CheckApplicationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class CheckCode(serializers.Serializer):
-    active_code=serializers.CharField(max_length=16)
-
-
 class GenerateCodeSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -22,12 +18,8 @@ class GenerateCodeSerializer(serializers.ModelSerializer):
 
 
 class CreateStoreSerializer(serializers.ModelSerializer):
-    code=serializers.CharField(max_length=12)
+    code=serializers.SlugField(source='info.CodeWarehouse.code',write_only=True)
 
     class Meta:
         model = models.CreateStore
         fields='__all__'
-
-    def create(self, validated_data):
-        code=validated_data.pop('code')
-        return super().create(validated_data)
