eed8c6acb185d7811bbdf05c40c4d9ec8570ba33
gongjinwei <jwakx@163.com>
2018-06-25 11:32:51 +0800

    返回申请ID

--- store/models.py
+++ store/models.py
@@ -2,12 +2,13 @@ import string
 
 from django.db import models
 from django.contrib.auth.models import User
+from index.models import Application
 
 # Create your models here.
 
 
 class CheckApplication(models.Model):
-    application=models.ForeignKey(to='index.Application',on_delete=models.CASCADE)
+    application=models.ForeignKey(to=Application,on_delete=models.CASCADE)
     checker=models.ForeignKey(to=User,on_delete=models.DO_NOTHING,editable=False)
     apply_status=models.SmallIntegerField(choices=((2,'打款验证中'),(4,'审核不通过')))
     opinion=models.CharField(max_length=255,help_text='审核意见')
@@ -15,10 +16,16 @@ class CheckApplication(models.Model):
 
 
 class CodeWarehouse(models.Model):
-    application=models.OneToOneField(to='index.Application',on_delete=models.DO_NOTHING)
-    code=models.CharField(max_length=16,editable=False)
+    application=models.OneToOneField(to=Application,on_delete=models.DO_NOTHING)
+    code=models.CharField(max_length=16,editable=False,unique=True)
     use_state=models.SmallIntegerField(choices=((0,'未使用'),(1,'已使用')),editable=False)
     active_user=models.ForeignKey(to=User,editable=False,null=True,blank=True,on_delete=models.SET_NULL)
     active_time=models.DateTimeField(auto_now=True,editable=False)
     create_time=models.DateTimeField(auto_now_add=True,editable=False)
+
+
+class CreateStore(models.Model):
+    application=models.OneToOneField(to=Application,on_delete=models.CASCADE)
+
+
 
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import Response,status
+from rest_framework.views import Response, status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
 
@@ -8,7 +8,7 @@ from register.viewset import CreateOnlyViewSet
 
 # Create your views here.
 
-from . import serializers,models
+from . import serializers, models
 
 
 class CheckApplicationViewSets(ModelViewSet):
@@ -18,12 +18,12 @@ class CheckApplicationViewSets(ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        application=serializer.validated_data['application']
-        if application.application_status==1:
-            application.application_status=serializer.validated_data['apply_status']
+        application = serializer.validated_data['application']
+        if application.application_status == 1:
+            application.application_status = serializer.validated_data['apply_status']
             application.save()
         else:
-            return Response('该状态无法被更改',status=status.HTTP_400_BAD_REQUEST)
+            return Response('该状态无法被更改', status=status.HTTP_400_BAD_REQUEST)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
@@ -35,9 +35,12 @@ class GenerateCodeView(CreateOnlyViewSet):
     permission_classes = (IsAdminUser,)
 
     def perform_create(self, serializer):
-        data={
-            'code':get_random_string(),
-            'use_state':0,
-            'active_user':self.request.user
+        code = get_random_string()
+        while models.CodeWarehouse.objects.filter(code=code).exists():
+            code = get_random_string()
+        data = {
+            'code': code,
+            'use_state': 0,
+            'active_user': self.request.user
         }
         serializer.save(**data)
