f72c7f3bac49b9de9ee612c8705ea2638af03e34
gongjinwei <jwakx@163.com>
2018-06-29 11:03:33 +0800

    普通商家权限

--- tools/permissions.py
+++ tools/permissions.py
@@ -6,18 +6,18 @@ from django.contrib.auth.models import Group
 class MerchantPermission(BasePermission):
     def has_permission(self, request, view):
 
-        group,created=Group.objects.get_or_create(defaults={"name":'merchant %s'%request.user.username},name='merchant %s'%request.user.username)
+        group,created=Group.objects.get_or_create(defaults={"name":'merchant0'},name='merchant0')
 
-        return request.user and request.user.is_authenticated and request.user.has_perm('store.change_stores',group)
+        return request.user and request.user.is_authenticated and request.user.has_perm('change_stores',group)
 
 
 class MerchantOrReadOnlyPermission(BasePermission):
     def has_permission(self, request, view):
-        group,created = Group.objects.get_or_create(defaults={"name": 'merchant %s'%request.user.username}, name='merchant %s'%request.user.username)
+        group,created = Group.objects.get_or_create(defaults={"name": 'merchant0'}, name='merchant0')
 
         return (
                 request.method in SAFE_METHODS or
                 request.user and
                 request.user.is_authenticated and
-                request.user.has_perm('store.change_stores', group)
+                request.user.has_perm('change_stores', group)
         )
