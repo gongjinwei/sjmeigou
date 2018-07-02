ce4809db86dc6d0fd73023dd26ecae282b02320b
gongjinwei <jwakx@163.com>
2018-07-02 10:48:57 +0800

    生成店铺二维码

--- tools/permissions.py
+++ tools/permissions.py
@@ -4,16 +4,21 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class MerchantPermission(BasePermission):
     def has_permission(self, request, view):
-
-        return request.user and request.user.is_authenticated and request.user.has_perm('store.change_stores',request.user.stores)
+        if request.user.stores:
+            return request.user and request.user.is_authenticated and request.user.has_perm('store.change_stores',request.user.stores)
+        else:
+            return False
 
 
 class MerchantOrReadOnlyPermission(BasePermission):
     def has_permission(self, request, view):
-
-        return (
-                request.method in SAFE_METHODS or
-                request.user and
-                request.user.is_authenticated and
-                request.user.has_perm('store.change_stores',request.user.stores)
-        )
+        if request.method in SAFE_METHODS:
+            return True
+        elif request.user.stores:
+            return (
+                    request.user and
+                    request.user.is_authenticated and
+                    request.user.has_perm('store.change_stores',request.user.stores)
+            )
+        else:
+            return False
