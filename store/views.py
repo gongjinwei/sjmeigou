765ac352283f9496d31ded4abc4e4f7235eca735
gongjinwei <jwakx@163.com>
2018-07-05 09:40:50 +0800

    商品分类中加入批量上架

--- store/views.py
+++ store/views.py
@@ -242,12 +242,12 @@ class GoodsTypeView(ListDeleteViewSet):
             good_type = models.GoodsType.objects.get(pk=pk)
             serializer = self.get_serializer(data=request.data)
             serializer.is_valid(raise_exception=True)
-            good_ids = serializer.validated_data.get('good_list', [])
-            put_on_sale_ids=serializer.validated_data.get('put_on_sale_list',[])
-            owner_good_ids = GoodDetail.objects.filter(owner=request.user)
-            if good_ids and good_ids in owner_good_ids and good_type.store_goods_type.store == request.user.stores:
+            good_ids = set(serializer.validated_data.get('good_list', []))
+            put_on_sale_ids=set(serializer.validated_data.get('put_on_sale_list',[]))
+            owner_good_ids = list(GoodDetail.objects.filter(owner=request.user).values_list('id',flat=True))
+            if good_ids and good_ids.issubset(owner_good_ids) and good_type.store_goods_type.store == request.user.stores:
                 GoodDetail.objects.filter(id__in=good_ids).update(good_type=good_type)
-            if put_on_sale_ids and put_on_sale_ids in owner_good_ids:
+            if put_on_sale_ids and put_on_sale_ids.issubset(owner_good_ids):
                 GoodDetail.objects.filter(id__in=put_on_sale_ids).update(state=0)
             return Response('ok')
         else:
