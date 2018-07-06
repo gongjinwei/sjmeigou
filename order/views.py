ffad1638805e640c576cd1c2edf33937b7f14ecb
gongjinwei <jwakx@163.com>
2018-07-06 10:33:58 +0800

    删除shopping car

--- order/views.py
+++ order/views.py
@@ -11,7 +11,7 @@ class ShoppingCarView(ModelViewSet):
     serializer_class = serializers.ShoppingCarItemSerializer
 
     def get_queryset(self):
-        if self.request.user.is_authencated:
+        if self.request.user.is_authenticated:
             queryset=models.ShoppingCarItem.objects.filter(user=self.request.user)
         else:
             return models.ShoppingCarItem.objects.none()
