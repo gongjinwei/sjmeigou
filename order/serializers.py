f71162022577c7d1d5548ee19153fe88049d924e
gongjinwei <jwakx@163.com>
2018-07-06 17:21:04 +0800

    限制优惠券金额

--- order/serializers.py
+++ order/serializers.py
@@ -7,30 +7,37 @@ from . import models
 
 
 class ShoppingCarItemSerializer(serializers.ModelSerializer):
-    sku_id=serializers.ReadOnlyField(source='sku.id')
-    title=serializers.ReadOnlyField(source='sku.color.good_detail.title')
-    price=serializers.ReadOnlyField(source='sku.price')
-    color=serializers.ReadOnlyField(source='sku.color.color_name')
-    size=serializers.ReadOnlyField(source='sku.size.size_name')
-    good_id=serializers.ReadOnlyField(source='sku.color.good_detail.id')
-    store_id=serializers.ReadOnlyField(source='sku.color.good_detail.store.id')
+    sku_id = serializers.ReadOnlyField(source='sku.id')
+    title = serializers.ReadOnlyField(source='sku.color.good_detail.title')
+    price = serializers.ReadOnlyField(source='sku.price')
+    color = serializers.ReadOnlyField(source='sku.color.color_name')
+    size = serializers.ReadOnlyField(source='sku.size.size_name')
+    good_id = serializers.ReadOnlyField(source='sku.color.good_detail.id')
+    store_id = serializers.ReadOnlyField(source='sku.color.good_detail.store.id')
 
     class Meta:
-        model=models.ShoppingCarItem
-        fields='__all__'
+        model = models.ShoppingCarItem
+        fields = '__all__'
 
     def create(self, validated_data):
-
         ModelClass = self.Meta.model
-        num=validated_data.get('num')
+        num = validated_data.get('num')
 
-        instance,created = ModelClass.objects.get_or_create(defaults=validated_data,sku=validated_data['sku'],user=validated_data['user'])
+        instance, created = ModelClass.objects.get_or_create(defaults=validated_data, sku=validated_data['sku'],
+                                                             user=validated_data['user'])
         if not created:
-            ModelClass.objects.filter(pk=instance.id).update(num=F('num')+num)
+            ModelClass.objects.filter(pk=instance.id).update(num=F('num') + num)
         return instance
 
 
+def check_discount(value):
+    if value % 5 != 0:
+        raise serializers.ValidationError('折扣面额必须是5的倍数')
+
+
 class CouponSerializer(serializers.ModelSerializer):
+    discount = serializers.IntegerField(validators=[check_discount])
+
     class Meta:
         model = models.Coupon
         fields = '__all__'
