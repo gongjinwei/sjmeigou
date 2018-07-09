857f5400523698ef7cfb4754621c8b1343d00d2f
gongjinwei <jwakx@163.com>
2018-07-09 10:51:45 +0800

    修改购物车bug

--- order/serializers.py
+++ order/serializers.py
@@ -38,24 +38,6 @@ class ShoppingCarItemSerializer(serializers.ModelSerializer):
             ModelClass.objects.filter(pk=instance.id).update(num=F('num') + num)
         return instance
 
-    def update(self, instance, validated_data):
-        instance= models.ShoppingCarItem.objects.get(shopping_car=instance.shopping_car,sku=instance.sku)
-        info = model_meta.get_field_info(instance)
-
-        # Simply set each attribute on the instance, and then save it.
-        # Note that unlike `.create()` we don't need to treat many-to-many
-        # relationships as being a special case. During updates we already
-        # have an instance pk for the relationships to be associated with.
-        for attr, value in validated_data.items():
-            if attr in info.relations and info.relations[attr].to_many:
-                field = getattr(instance, attr)
-                field.set(value)
-            else:
-                setattr(instance, attr, value)
-        instance.save()
-
-        return instance
-
 
 class ShoppingCarSerializer(serializers.ModelSerializer):
     items = ShoppingCarItemSerializer(many=True,read_only=True)
--- order/views.py
+++ order/views.py
@@ -30,6 +30,27 @@ class ShoppingCarItemView(ModelViewSet):
             queryset = models.ShoppingCarItem.objects.none()
         return queryset
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        try:
+            instance = models.ShoppingCarItem.objects.filter(shopping_car=instance.shopping_car,sku=request.data.get('sku'))[0]
+        except:
+            pass
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+
+
 
 class ShoppingCarView(ListOnlyViewSet):
     serializer_class = serializers.ShoppingCarSerializer
