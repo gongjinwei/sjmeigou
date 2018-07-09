13222ea401fc1587b088ed01fa2cbf66f48a32d1
gongjinwei <jwakx@163.com>
2018-07-09 15:08:47 +0800

    修复删除分类错误

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import Response, status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 
 from django.utils.crypto import get_random_string
 from django.conf import settings
@@ -257,3 +258,27 @@ class GoodsTypeView(ListDeleteViewSet):
             elif op == 'lower':
                 GoodDetail.objects.filter(id__in=put_on_sale_ids).update(state=1)
         return Response('ok')
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_destroy_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def get_destroy_object(self):
+        queryset=models.GoodsType.objects.all()
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+                'Expected view %s to be called with a URL keyword argument '
+                'named "%s". Fix your URL conf, or set the `.lookup_field` '
+                'attribute on the view correctly.' %
+                (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
