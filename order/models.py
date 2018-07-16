0d818bc67e25697953ace4467150679dc355668d
gongjinwei <jwakx@163.com>
2018-07-16 14:38:42 +0800

    合并订单与下单

--- order/models.py
+++ order/models.py
@@ -133,6 +133,7 @@ class UnifyOrder(models.Model):
     user_message=models.CharField(max_length=255,default='',blank=True)
     user = models.ForeignKey(to=User,on_delete=models.DO_NOTHING,editable=False)
     create_time = models.DateTimeField(auto_now_add=True)
+    address = models.ForeignKey(to='ReceiveAddress',on_delete=models.SET_NULL,null=True)
     deliver_server = models.ForeignKey(to='goods.GoodDeliver', on_delete=models.DO_NOTHING, null=True)
     deliver_payment = models.DecimalField(max_digits=30,decimal_places=2,default=Decimal(0.00))
     update_time = models.DateTimeField(auto_now=True)
