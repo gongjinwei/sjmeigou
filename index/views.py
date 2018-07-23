e16a78fe121e1622c3be10fb8a5986cff1ff52b8
gongjinwei <jwakx@163.com>
2018-07-23 11:36:35 +0800

    店铺申请换地

--- index/views.py
+++ index/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.views import Response
+from rest_framework.views import Response,status
 
 from tools.viewset import ListOnlyViewSet
 
@@ -43,9 +43,15 @@ class ApplicationViewSets(ModelViewSet):
     serializer_class = serializers.ApplicationSerializer
     queryset = models.Application.objects.all()
 
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         if models.Application.objects.filter(application_user=self.request.user).exists():
             return Response({'code':2001,'msg':'你的信息已提交，请不要重复申请'})
+        self.perform_create(serializer)
+        return Response({'code':1000,'msg':'申请成功'})
+
+    def perform_create(self, serializer):
         serializer.save(application_user=self.request.user)
 
     def get_queryset(self):
