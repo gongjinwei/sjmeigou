5064809f2a6c9cf9716e6896f31b1284f5b5785c
gongjinwei <jwakx@163.com>
2018-07-27 10:52:22 +0800

    记录物流送达时间

--- order/models.py
+++ order/models.py
@@ -282,5 +282,5 @@ class DwdOrder(models.Model):
 
 class DwdOrderComment(models.Model):
     is_satisfied = models.BooleanField(default=True)
-    satisfied_reasons=models.ManyToManyField(to='platforms.DeliveryReason',on_delete=models.SET_NULL,null=True)
+    satisfied_reasons=models.ManyToManyField(to='platforms.DeliveryReason')
     comment_time = models.DateTimeField(auto_now_add=True)
